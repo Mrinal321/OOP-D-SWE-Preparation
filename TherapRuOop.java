d5fdb38eef986b60a4e2b7be8fd7dfc945743e43
Mrinal Kanti Howlader <mrinal.ru.cse@gmail.com>
2025-08-22 12:12:24 +0600

    Therap RU recrutement oop written question.

---
+++ TherapRuOop.java
@@ -0,0 +1,36 @@
+abstract class Shape{
+    abstract void area();
+}
+
+class Rectangle extends Shape{
+    private double height, weidth;
+    Rectangle(double height, double weidth){
+        this.height = height;
+        this.weidth = weidth;
+    }
+    void area(){
+        double area =  height*weidth;
+        System.err.println("Area of Rectangle: " + area);
+    }
+}
+
+class Square extends Shape{
+    private double lenght;
+    Square(double lenght){
+        this.lenght = lenght;
+    }
+    void area(){
+        double area =  lenght*lenght;
+        System.err.println("Area of Square: " + area);
+    }
+}
+
+
+public class TherapRuOop {
+    public static void main(String[] args) {
+        Shape rec = new Rectangle(3, 2);
+        rec.area();
+        Shape sqr = new Square(4);
+        sqr.area();
+    }
+}
